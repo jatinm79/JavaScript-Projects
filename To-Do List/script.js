840abefb648c0db7cc7f0f8ef7e30116a68a34c2
Jatin Mahajan <jatinmahajan2979@gmail.com>
2025-06-13 23:57:28 +0530

    Update script.js

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -2,6 +2,7 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask(){
+    // Check if input is empty
     if(inputBox.value === ''){
         alert("You must write something!");
     }
